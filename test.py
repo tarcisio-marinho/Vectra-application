552da6af07651b1377bcc992044f177091dd62e3
tarcisio-marinho <tarcisio_marinho09@hotmail.com>
2019-06-16 20:14:04 -0300

    removes test

--- test.py
+++
@@ -1,6 +0,0 @@
-import subprocess
-terminal_output = subprocess.Popen("sudo ls", shell=True, stdin=subprocess.PIPE,
-                                         stdout=subprocess.PIPE,
-                                         stderr=subprocess.PIPE)
-                                        
-print(terminal_output.stdout.readlines())
