8a079302c1c347c62b90c2df6b807611411c73c6
tarcisio-marinho <tarcisio_marinho09@hotmail.com>
2019-06-16 01:09:54 -0300

    checks if commands from requeriments are installed

---
+++ requeriments.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+echo 'Installing dependencies';
+
+if ! [ -x "$(command -v pip3)" ]; then
+  echo 'Error: pip3 is not installed.' >&2
+  exit 1
+else
+    sudo pip3 install pyinstaller
+fi
+
+if ! [ -x "$(command -v apt-get)" ]; then
+  echo 'Error: apt-get is not installed.' >&2
+  exit 1
+else
+    sudo apt install python3-tk
+fi
+
+if ! [ -x "$(command -v dnf)" ]; then
+  echo 'Error: dnf is not installed.' >&2
+  exit 1
+else
+    sudo dnf install python3-tk
+fi
