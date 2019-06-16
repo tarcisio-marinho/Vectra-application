24bc16828bbaf4fb707a7e1fa3ed510a399e4b4b
tarcisio-marinho <tarcisio_marinho09@hotmail.com>
2019-06-15 23:52:28 -0300

    fixes logos exibition

--- app.py
+++ app.py
@@ -13,6 +13,8 @@ def shutdown():
 def restart():
     pass
 
+def terminal():
+    pass
 
 
 window = Tk()
@@ -29,22 +31,24 @@ txt = Entry(window,width=10)
  
 txt.grid(column=1, row=0)
 
+
 shutdown_button = Button(window, command = shutdown)
+logo = PhotoImage(file = resource_path('assets/shutdown.png')).subsample(10, 10)
+shutdown_button.config(image=logo)
 
-logo = PhotoImage(file = resource_path('assets/terminal.png'))
-small_logo = logo.subsample(10, 10)
-
-
-shutdown_button.config(image = small_logo , compound = BOTTOM )
-
-''' restart_button = Button(window, text="Restart", command=restart)
-logo = PhotoImage(file = 'assets/restart.png')
-small_logo = logo.subsample(5, 5)
-restart_button.config(image = small_logo , compound = BOTTOM )
- '''
+restart_button = Button(window, command=restart)
+restart_logo = PhotoImage(file = resource_path('assets/restart.png')).subsample(10, 10)
+restart_button.config(image = restart_logo)
 
 
-shutdown_button.grid(column=2, row=0)
-''' restart_button.grid(column=10, row=0)
- ''' 
+terminal_button = Button(window, command=terminal)
+terminal_logo = PhotoImage(file = resource_path('assets/terminal.png')).subsample(5, 5)
+terminal_button.config(image = terminal_logo)
+
+
+
+shutdown_button.grid(column=10, row=200)
+restart_button.grid(column=20, row=200)
+terminal_button.grid(column=30, row=200)
+
 window.mainloop()
